5e8d2e944b1e34ac55ae1330a826e264b1cdacf4
Gleidson8989 <gleidson108.rg@gmail.com>
2026-04-20 20:29:56 -0300

    exercicio 01 Regis

---
+++ pratica_01/regis-gleidson/ex01.py
@@ -0,0 +1,5 @@
+nome = input("Qual o seu nome? ")
+idade = int(input("Qual o ano do seu nascimento? "))
+altura = float(input("Qual a sua altura? Atonio"))
+
+print("Olá, " + nome + "! Você tem " + str(2026 - idade) + " anos e mede " + str(altura) + " metros de altura.")
